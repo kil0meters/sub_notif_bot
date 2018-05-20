587133f1bb902139a47418ed859691e254881dd6
kilometers <kil0meters@pm.me>
2018-05-20 12:43:25 -0700

    update

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 function submit() {
     var data = {};
-    var elements = document.getElementsByClassName("subreddit-input");
+    var els = document.getElementsByClassName("form-input");
     var subreddits = [];
-    for (var i = 0; i < elements.length; i++) {
-        if (elements[i].value) {
-            subreddits.push(elements[i].value.trim())
-        }
-    }
-    var elements = document.getElementsByClassName("filter-input");
-    var filters = [];
-    for (var i = 0; i < elements.length; i++) {
-        if (elements[i].value) {
-            filters.push(elements[i].value.trim())
+    for (var i = 0; i < els.length; i++) {
+        subreddit = document.getElementById("subreddit-input" + i).value;
+        filters = document.getElementById("filter-input" + i).value;
+        if(subreddit) {
+            subreddits.push([subreddit, filters])
         }
     }
     data["subreddits"] = subreddits;
-    data["filters"] = filters;
     data = JSON.stringify(data);
 
     url = "https://reddit.com/message/compose/?to=sub_notif_bot&subject=update%20settings&message=" + encodeURIComponent(data);
